f2f020f9a95ff85464f74913848782253c64bfb0
Ashley Lee <72521813+ylee73@users.noreply.github.com>
2021-02-03 14:45:54 -0800

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   print("Height="+height);
 
 
-  //Set ellipse X pos
+ //Set ellipse X pos
   ellipseXPos = width -250;
  }
 
@@ -27,35 +27,33 @@ function draw() {
   drawRects();
   drawCircles();
   drawEllipse();
+  noStroke();
   drawTri();
 
   fill(204);
   quad(100,100,200,100,200,250,140,230);
-
+//white half of circle
   fill(255);
   arc(480,500,100,100,PI,TWO_PI);
-
+//larger arc 
   fill(100,23,45);
   arc(480,650,200,200,0,PI + QUARTER_PI);
 
-  
+ //gray square with lining
   fill(120,120,120);
   stroke(23);
   strokeWeight(7);
   square(800,600,120);
-
-
 }
 
 function drawRects(){
 //red center rectangle
-
   fill(255,0,0);
   stroke(23);
   strokeWeight(5);
   rect(width/2,height/2,200,100);
 
-//blueish square
+//blueish rectangle with lining
   fill(23,200,200);
   stroke(200);
   strokeWeight(5);
@@ -63,22 +61,27 @@ function drawRects(){
 }
 
 function drawEllipse(){
-	//green ellipse
+//green ellipse
   fill(10,200,20);
   ellipse(ellipseXPos,ellipseYPos,100,80); 
 
-// ellipse
+// yellow ellipse
   fill(210,200,20);
   ellipse(ellipseXPos,ellipseYPos + ellipseDist,30,50); 
 
+//orange ellipse 
+  fill(234,100,30);
+  stroke(10,200,20);
+  strokeWeight(3);
+  ellipse(ellipseXPos +ellipseDist, ellipseYPos,50,40);
 }
 function drawCircles(){
-	//middle circle 
+//middle circle on top of red rectangle
   fill(0,102,255);
   strokeWeight(1);
   circle(width/2,height/2,20);
 }
-
+//mini white triangle
 function drawTri(){
 	fill(240);
 	triangle(300,100,340,100,310,80);
